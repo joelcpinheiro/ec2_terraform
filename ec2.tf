b09dee79b20f99b1718373a92bc58aff521f3ecb
Joel Pinheiro <38402352+joelcpinheiro@users.noreply.github.com>
2021-04-08 23:20:49 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,4 +10,22 @@ resource "aws_instance" "server" {
   }
 }
 
+resource "aws_security_group" "sg" {
+  name        = "sg"
+  description = "Allow TCP/80 & TCP/22"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
