9a746dcc48f9932684d976c20cfe4cc829d72ea5
Joel Pinheiro <joelcostapinheiro@gmail.com>
2021-04-20 02:21:35 +0000

    update to three machines

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "server" {
+  count         = var.instance_count
   ami           = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
   vpc_security_group_ids      = [aws_security_group.sg.id]
  
   tags = {
-    Name        = var.name
+    Name        = "K8S-${count.index + 1}"
     Environment = var.env
     Provisioner = "Terraform"
   }
@@ -14,19 +15,19 @@ resource "aws_instance" "server" {
 # Creating a Security Group with these allowing ports
 
 resource "aws_security_group" "sg" {
-  name        = "sg"
+  name        = "sg_new"
   description = "Allow TCP/80 & TCP/22"
   ingress {
-    description = "Allow SSH traffic"
+    description = "Allow access to por 22"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["187.18.129.236/32"]
   }
   ingress {
-    description = "Allow HTTPD traffic"
-    from_port   = 80
-    to_port     = 80
+    description = "Allow HTTP traffic on port 8081"
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["187.18.129.236/32"]
   }
--- variables.tf
+++ variables.tf
@@ -3,9 +3,8 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "name" {
-  description = "Name of the Application"
-  default = "server01"
+variable "instance_count" {
+  default = "3"
 }
 
 variable "env" {
