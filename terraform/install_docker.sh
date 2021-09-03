806a6a29d5dafaac1564ff2d81a044002539e3f2
Joel Pinheiro <joelcostapinheiro@gmail.com>
2021-09-03 11:09:19 -0300

    update data

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -5,7 +5,4 @@ sudo service docker start
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-mkdir prod ; cd prod
-sudo wget -O docker-compose.yml https://raw.githubusercontent.com/joelcpinheiro/terraform_ec2/main/docker-compose.yml
-sudo docker-compose up -d
 sudo yum update -y 
